fcf3eb81d346c89d4a6c2cf2e6c91900d5dacf15
rafi-windows <rafi_n_o@github.com>
2020-12-15 14:04:31 +0700

    membuat proses register customer

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+// import router
+import router from './router'
+
+// import store vuex
+import store from './store'
+
 const app = createApp(App)
+
+// gunakan router di vue js dengan plugin 'use'
+app.use(router)
+
+// gunakan store di vue js dengan plugin 'use'
+app.use(store)
 
 // define mixins for global function
 app.mixin({
